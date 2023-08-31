478ec8b369f830d5ec4a6a18fc78cf9cfc6d53a8
JoaoDevSm <joaobarbosasm@gmail.com>
2023-08-31 14:44:30 -0300

    Pag42

--- my-project/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/NavigationActivity.kt
+++ my-project/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/NavigationActivity.kt
@@ -2,8 +2,11 @@ package tech.salvatore.livro_android_kotlin_paulo_salvatore
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class NavigationActivity : AppCompatActivity() {
+    private val navigationViewModel : NavigationViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.navigation_activity)
